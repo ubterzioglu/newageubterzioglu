a1470a91f0a76fd5b209a8317b27bff97428e988
Baris Terzioglu, Umut <Umut.Baris-Terzioglu@adesso-insurance-solutions.de>
2026-03-22 09:20:34 +0100

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import AdminCards from "./pages/AdminCards";
 import Cards from "./pages/Cards";
 
 const queryClient = new QueryClient();
@@ -20,8 +18,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/cards" element={<Cards />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<AdminCards />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
